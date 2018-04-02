91b2aa47e6ed2e2e7be3de8059d4309c20789990
Ryan Doane <rdoane@mail.csuchico.edu>
2018-04-02 11:46:08 -0700

    turned randomizer class into a simpleton

--- rand.cpp
+++ rand.cpp
@@ -5,21 +5,28 @@
 #include <iostream>
 using namespace std;
 
+Randomizer* Randomizer::rng = NULL;
+
 Randomizer::Randomizer(){
   srand(time(NULL));
 }
 
+Randomizer* Randomizer::instance(){
+  if(rng == NULL)
+     rng = new Randomizer();
+  return rng;
+}
 
 unsigned int Randomizer::randomBetween(unsigned int minimum, unsigned int maximum){
   return rand() % maximum + minimum;
 }
 
 int main(){
-  Randomizer rng;
-  int x = rng.randomBetween(0,10);
+  Randomizer *rng = Randomizer::instance();
+  int x = rng->randomBetween(0,10);
   for(int i = 0; i < 10; i++){
     cout << x << endl;
-    x = rng.randomBetween(0,10);
+    x = rng->randomBetween(0,10);
   }
   return 0;
 }
--- rand.h
+++ rand.h
@@ -5,9 +5,10 @@ using namespace std;
 
 class Randomizer{
   public:
+    static Randomizer *instance();
     unsigned int randomBetween(unsigned int minimum, unsigned int maximum);
     Randomizer();
   private:
-    
+    static Randomizer *rng;
 };
 #endif  
